9cdc44430bd0ff733000d6f7807350dbc3016e1d
강성원 책임 아키텍처컨설팅팀 (swonkang@lgcns.com <66276@CORP.LGCNS.COM>
2019-12-21 14:18:02 +0900

    Inital Commited.

---
+++ src/main/java/com/example/CustomerServiceCenter/CustomerServiceCenterRepository.java
@@ -0,0 +1,6 @@
+package com.example.CustomerServiceCenter;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CustomerServiceCenterRepository extends PagingAndSortingRepository<CustomerServiceCenter, Long>{
+}
